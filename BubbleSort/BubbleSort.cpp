cb860fc7828678ce2bd75e46532621ac45339bca
martinez415 <gigimartinez415@gmail.com>
2023-10-11 14:18:03 -0500

    Final with updated searches

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -22,23 +22,62 @@ void BubbleSort(int x[], int length) {
 
 }
 
+/*
+int LinearSearch(int x[], int length, int n) {
+    for (int i = 0; i < length; i++) {
+        if (x[i] == n) {
+            return i;
+        }
+        return -1;
+    }
+}
+*/
+
+
 int main()
 {
-    int x[] = { 4, 2, 65, 78, 99, 21, 6, 79, 78, 100 };
+    // an array of integers
+    int x[] = { 4, 2, 65, 78, 99, 21, 6, 79, 78, 100, 34, 79, 24, 56, 95, 7, 93 };
+    int key = 0;
+    int attempts = 1;
+    bool found = false;
 
     // in c++ get array size by size of array divided by size of int
     int xLength = sizeof(x) / sizeof(int);
 
+    // display array before sorting algorithm
     cout << "Original Array" << endl;
     for (int i = 0; i < xLength; i++) {
         cout << x[i] << " ";
     }
     cout << endl;
 
+    // pass array through BubbleSort function
     BubbleSort(x, xLength);
+
+    // display array after sorting algorithm
     cout << "Sorted Array" << endl;
     for (int i = 0; i < xLength; i++) {
         cout << x[i] << " ";
     }
     cout << endl;
+
+    cout << "Enter a number to search the array: ";
+    cin >> key;
+
+    for (int i = 0; i < xLength; i++) {
+        if (x[i] == key) {
+            found = true;
+            break;
+        }
+        attempts++;
+    }
+    if (!found) {
+        cout << "The number entered does not exist." << endl;
+    }
+    else {
+        cout << "The number was found. It took " << attempts << " of attempts to find." << endl;
+    }
+
+   
 }
